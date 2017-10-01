2b95f2c429df8935051ed3dcf0db345a9b220d92
Litao Z <zhaol8@rpi.edu>
2017-10-01 11:51:22 -0400

    Added Machine Learning Recipe #1 with comments

---
+++ Machine Learning Recipies/Recipe1.py
@@ -0,0 +1,41 @@
+# Recipe1.py
+# 
+# Categorizes apple and oranges based on features
+# Info provided in the table below:
+#
+# | Weight | Texture | Label  |
+# |--------|---------|--------|
+# | 150g   | Bumpy   | Orange |
+# | 170g   | Bumpy   | Orange |
+# | 140g   | Smooth  | Apple  |
+# | 130g   | Smooth  | Apple  |
+#
+# Tutorial on using machine learning from Google Developers
+# https://www.youtube.com/watch?v=cKxRvEZd3Mw
+#
+# Additional comments and notes written by LZ
+# Updated 10/1/17
+#
+
+
+from sklearn import tree
+
+# Supervised Learning Recipe 
+# Step 1: Collect Training Data
+features = [
+    [150, 0],  # [weight(g), smooth(bool)]
+    [170, 0],
+    [140, 1],
+    [130, 1]
+    ]
+labels = [0, 0, 1, 1]  # Labels, 0 = orange, 1 = apple
+
+# Step 2: Train Classifer
+# For this tutorial we will use "Decision Tree"
+clf = tree.DecisionTreeClassifier()  # variable name is "classifer"
+clf = clf.fit(features, labels)
+
+# Step 3: Make Predictions
+# Predict an orange-like (heavier and bumpy)
+print(clf.predict([[160, 0]]))
+assert clf.predict([[160, 0]]) == [0], "Should predict an orange"
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # LearningMachineLearning
 Me copying and trying out sci-kit learn and tensor flow
+
+## File Structure
+### Machine Learning Recipies
+https://www.youtube.com/watch?v=cKxRvEZd3Mw
+Checked in: 10/1/17
